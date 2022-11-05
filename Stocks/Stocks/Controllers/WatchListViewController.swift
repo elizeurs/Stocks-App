b2e2ebd09185dd53d0a9be326f7894f29538318e
Elizeu RS <13ders@gmail.com>
2022-11-05 15:57:29 -0300

    Setting Up Project

---
+++ Stocks/Stocks/Controllers/WatchListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Stocks
+//
+//  Created by Elizeu RS on 05/11/22.
+//
+
+import UIKit
+
+class WatchListViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
