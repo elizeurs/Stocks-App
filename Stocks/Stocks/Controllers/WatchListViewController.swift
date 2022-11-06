937814d7b5e0d9c31cc3e1776c6791f93aa46711
Elizeu RS <13ders@gmail.com>
2022-11-06 17:08:03 -0300

    Search Controller

--- Stocks/Stocks/Controllers/WatchListViewController.swift
+++ Stocks/Stocks/Controllers/WatchListViewController.swift
@@ -13,6 +13,33 @@ class WatchListViewController: UIViewController {
     super.viewDidLoad()
 //    view.backgroundColor = .systemRed
     view.backgroundColor = .systemBackground
+    setUpSearchController()
+  }
+  
+  private func setUpSearchController() {
+    let resultVC = SearchResultsViewController()
+    let searchVC = UISearchController(searchResultsController: resultVC)
+    searchVC.searchResultsUpdater = self
+    navigationItem.searchController = searchVC
+  }
+}
+
+extension WatchListViewController: UISearchResultsUpdating {
+  func updateSearchResults(for searchController: UISearchController) {
+    guard let query = searchController.searchBar.text,
+    let resultsVC = searchController.searchResultsController as? SearchResultsViewController,
+    !query.trimmingCharacters(in: .whitespaces).isEmpty else {
+      return
+    }
+    
+    // Optimize to reduce number of searches for when user stops typing
+    
+    // Call API to search
+    
+    // Update results controller
+    
+    
+    print(query)
   }
 }
 
