0576c2785839b1941835a63aa524b7267f7576d9
Elizeu RS <13ders@gmail.com>
2022-12-04 20:24:15 -0300

    Create Stock Detail View

--- Stocks/Stocks/Controllers/StockDetailsViewController.swift
+++ Stocks/Stocks/Controllers/StockDetailsViewController.swift
@@ -5,6 +5,7 @@
 //  Created by Elizeu RS on 05/11/22.
 //
 
+import SafariServices
 import UIKit
 
 class StockDetailsViewController: UIViewController {
@@ -15,6 +16,15 @@ class StockDetailsViewController: UIViewController {
   private let symbol: String
   private let companyName: String
   private var candleStickData: [CandleStick]
+  
+  private let tableView: UITableView = {
+    let table = UITableView()
+    table.register(NewsHeaderView.self, forHeaderFooterViewReuseIdentifier: NewsHeaderView.identifier)
+    table.register(NewsStoryTableViewCell.self, forCellReuseIdentifier: NewsStoryTableViewCell.identifier)
+    return table
+  }()
+  
+  private var stories: [NewsStory] = []
   
   // MARK: - Init
   
@@ -39,6 +49,131 @@ class StockDetailsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
       view.backgroundColor = .systemBackground
+      title = companyName
+      setUpCloseButton()
+      // Show view
+      setUpTable()
+      // Financial Data
+      fetchFinancialData()
+      fetchNews()
+      // Show Chart/Graph
+      // Show News
     }
+  
+  override func viewDidLayoutSubviews() {
+    super.viewDidLayoutSubviews()
+    tableView.frame = view.bounds
+  }
+  
+  // MARK: - Private
+  
+  private func setUpCloseButton() {
+    navigationItem.rightBarButtonItem = UIBarButtonItem(
+      barButtonSystemItem: .close,
+      target: self,
+      action: #selector(didTapClose)
+    )
+  }
+  
+  @objc private func didTapClose() {
+    dismiss(animated: true, completion: nil)
+  }
+  
+  private func setUpTable() {
+    view.addSubview(tableView)
+    tableView.delegate = self
+    tableView.dataSource = self
+    tableView.tableHeaderView = UIView(
+      frame: CGRect(x: 0, y: 0, width: view.width, height: (view.width * 0.7) + 100)
+    )
+  }
+  
+  private func fetchFinancialData() {
+    // Fetch candle sticks if needed
+    
+    // Fetch financial metrics
+    
+    renderChart()
+  }
+  
+  private func fetchNews() {
+    APICaller.shared.news(for: .company(symbol: symbol)) { [weak self] result in
+      switch result {
+      case .success(let stories):
+        DispatchQueue.main.async {
+          self?.stories = stories
+          self?.tableView.reloadData()
+        }
+      case .failure(let error):
+        print(error)
+      }
+    }
+  }
 
+  private func renderChart() {
+    
+  }
+}
+
+extension StockDetailsViewController: UITableViewDelegate, UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return stories.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    // Add to watchlist
+    guard let cell = tableView.dequeueReusableCell(withIdentifier: NewsStoryTableViewCell.identifier, for: indexPath) as? NewsStoryTableViewCell else {
+      
+      fatalError()
+       }
+    cell.configure(with: .init(model: stories[indexPath.row]))
+    return cell
+  }
+  
+  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    return NewsStoryTableViewCell.preferredHeight
+  }
+  
+  func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+    guard let header = tableView.dequeueReusableHeaderFooterView(withIdentifier: NewsHeaderView.identifier) as? NewsHeaderView else {
+      return nil
+    }
+    
+    header.delegate = self
+    header.configure(with: .init(
+      title: symbol.uppercased(),
+      shouldShowAddButton: !PersistenceManager.shared.watchlistContains(symbol: symbol)
+    )
+    )
+    return header
+  }
+  
+  func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+    return NewsHeaderView.preferredHeight
+  }
+  
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    tableView.deselectRow(at: indexPath, animated: true)
+    guard let url = URL(string: stories[indexPath.row].url) else { return }
+    let vc = SFSafariViewController(url: url)
+    present(vc, animated: true)
+  }
+}
+
+extension StockDetailsViewController: NewsHeaderViewDelegate {
+  func newsHeaderViewDidTapAddButton(_ headerView: NewsHeaderView) {
+    headerView.button.isHidden = true
+    PersistenceManager.shared.addToWatchlist(
+      symbol: symbol,
+      companyName: companyName
+    )
+    
+    let alert = UIAlertController(
+      title: "Added to Watchlist",
+      message: "We've added \(companyName) to your watchlist.",
+      preferredStyle: .alert
+    )
+    alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel))
+    present(alert, animated: true)
+  }
 }
